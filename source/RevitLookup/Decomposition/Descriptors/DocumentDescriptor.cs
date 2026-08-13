c3bc0d487bd905ff52f4b3fdd42fe4eda9e0c403
Nice3point <nice3point@gmail.com>
2026-08-13 15:48:38 +0300

    [SDK] Update preprocessor directives

--- source/RevitLookup/Decomposition/Descriptors/DocumentDescriptor.cs
+++ source/RevitLookup/Decomposition/Descriptors/DocumentDescriptor.cs
@@ -1,24 +1,30 @@
 // Copyright (c) Lookup Foundation and Contributors
-// 
+//
 // Permission to use, copy, modify, and distribute this software in
 // object code form for any purpose and without fee is hereby granted,
 // provided that the above copyright notice appears in all copies and
 // that both that copyright notice and the limited warranty and
 // restricted rights notice below appear in all supporting
 // documentation.
-// 
+//
 // THIS PROGRAM IS PROVIDED "AS IS" AND WITH ALL FAULTS.
 // NO IMPLIED WARRANTY OF MERCHANTABILITY OR FITNESS FOR A PARTICULAR USE IS PROVIDED.
 // THERE IS NO GUARANTEE THAT THE OPERATION OF THE PROGRAM WILL BE
 // UNINTERRUPTED OR ERROR FREE.
 
-#if !REVIT2025_OR_GREATER
-using Autodesk.Revit.DB.Macros;
-#endif
 using Autodesk.Revit.DB.Fabrication;
 using Autodesk.Revit.DB.Lighting;
 using LookupEngine.Abstractions.Configuration;
 using LookupEngine.Abstractions.Decomposition;
+
+#if !REVIT2025_OR_GREATER
+using Autodesk.Revit.DB.Macros;
+#endif
+
+#if REVIT2023_OR_GREATER
+using Autodesk.Revit.DB.Structure;
+#endif
+
 #if REVIT2026_OR_GREATER
 using Autodesk.Revit.DB.ExternalData;
 #endif
